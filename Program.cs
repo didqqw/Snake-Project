23d50412347de7f0dc93f65fe10ca8520c39ea47
didqqw <carter.ecward@gmail.com>
2021-02-07 01:02:58 +0200

    mini-Obu4alka

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Hor_Line line = new Hor_Line(5, 10, 8, '+');
+            line.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
